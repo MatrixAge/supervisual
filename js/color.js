245223f9266ee86378c6c55ed5510ecbe3e916d1
ATURBO <xiewendao123@foxmail.com>
2017-06-29 12:01:30 +0800

    Update color.js

--- js/color.js
+++ js/color.js
@@ -82,12 +82,15 @@ setInterval(function() {
 	
 	var second2 = Math.floor(aud.duration % 60);
 
-	if(second2 < 10) {
-		
-		dtime.innerText = "0" + Math.floor(aud.duration / 60) + ":0" + Math.floor(aud.duration % 60);
-	} 
-	else {		
-		dtime.innerText = "0" + Math.floor(aud.duration / 60) + ":" + Math.floor(aud.duration % 60);				
+	if(aud.duration !== NaN) {
+		if(second2 < 10) {
+
+			dtime.innerText = "0" + Math.floor(aud.duration / 60) + ":0" + Math.floor(aud.duration % 60);
+		} else {
+			dtime.innerText = "0" + Math.floor(aud.duration / 60) + ":" + Math.floor(aud.duration % 60);
+		}
+	}else{
+		dtime.innerText ="00:00";
 	}
 
 }, 30)
